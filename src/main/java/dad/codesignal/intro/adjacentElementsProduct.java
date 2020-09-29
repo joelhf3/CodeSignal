94b3876efed0cf3d64e4c3a3ce3a5dc394c75979
joelhf3 <joel_hf3@hotmail.com>
2020-09-29 11:43:10 +0100

    Create adjacentElementsProduct.java

---
+++ src/main/java/dad/codesignal/intro/adjacentElementsProduct.java
@@ -0,0 +1,23 @@
+package dad.codesignal.intro;
+
+public class adjacentElementsProduct {
+
+	
+	public static int adjacentElementsProduct(int[] inputArray) {
+		
+		int num1;
+		int num2;
+		
+		for(int i = 0; i < inputArray.length; i++)
+		{
+			
+		}
+
+	}
+
+	
+	public static void main(String[] args) {
+
+	}
+
+}
